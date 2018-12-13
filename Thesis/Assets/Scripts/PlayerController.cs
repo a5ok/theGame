127d2ae78fc27cbe3fdca99757f095d8aee8d385
Luca Assolari <luca.assolari405@gmail.com>
2018-12-13 17:11:02 +0100

    Primi passi
    Andiamocene

---
+++ Thesis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    private Rigidbody2D myRigidbody;
+    private bool grounded;
+    public LayerMask whatIsGround;
+    private Collider2D myCollider;
+    private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+        myCollider = GetComponent<Collider2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+
+
+        // Il player salta quando viene toccato lo schermo
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+            myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+
+        myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+
+
+
+    }
+}
---
+++ Thesis/Assets/Scripts/WaitForPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForPlayer : MonoBehaviour {
+
+    [SerializeField] public GameObject player;
+    [SerializeField] public GameObject start;
+    private bool waitingToStartGame = true;
+
+	// Use this for initialization
+	void Start () {
+        player.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(waitingToStartGame && Input.GetKeyDown(KeyCode.Space))
+        {
+            waitingToStartGame = false;
+            player.SetActive(true);
+            start.SetActive(false);
+        }
+        
+    }
+}
