b945be478e8b439230eae1b10c57595b3ec4ef19
Luca Assolari <luca.assolari405@gmail.com>
2018-12-17 15:13:30 +0100

    Animazione salto e font

--- Thesis/Assets/Scripts/PlayerController.cs
+++ Thesis/Assets/Scripts/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 
         grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+        myAnimator.SetBool("Ground", grounded);
         myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
 
         // Il player salta quando viene toccato lo schermo
@@ -32,6 +33,7 @@ public class PlayerController : MonoBehaviour {
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
 
         myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+        myAnimator.SetFloat("vSpeed", myRigidbody.velocity.y);
 
     }
 
