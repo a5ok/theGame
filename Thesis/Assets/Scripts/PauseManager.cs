9a662266c6d63c1def034ee5702a8709f6c45f28
ser-piatti <45850350+ser-piatti@users.noreply.github.com>
2019-02-15 14:46:13 +0100

    pausa

---
+++ Thesis/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool paused = false;
+    public GameObject pauseText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2") && !paused)
+            paused = true;
+
+        else if (paused)
+        {
+            Time.timeScale = 0f;
+            pauseText.SetActive(true);  
+            if (Input.GetButtonDown("Fire2"))
+            {
+                Time.timeScale = 1f;
+                paused = false;
+                pauseText.SetActive(false);
+            }
+            if (Input.GetButtonDown("Jump") && paused)
+            {
+                Time.timeScale = 1f;
+                paused = false;
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+
+       
+
+    }
+}
