97cde1ff2670fee1a5180a787265c859350966c1
Luca Assolari <luca.assolari405@gmail.com>
2019-02-28 16:59:50 +0100

    Login

---
+++ Thesis/Assets/Scripts/HashFunction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Security.Cryptography;
+using System.Text;
+
+public class HashFunction : MonoBehaviour
+{
+    public static string Sha256(string randomString)
+    {
+        var crypt = new System.Security.Cryptography.SHA256Managed();
+        var hash = new System.Text.StringBuilder();
+        byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString));
+
+        foreach (byte theByte in crypto)
+        {
+            hash.Append(theByte.ToString("x2"));
+        }
+
+        return hash.ToString();
+    }
+}
---
+++ Thesis/Assets/Scripts/Login.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public GameObject InputFieldEmail;
+    public GameObject InputFieldPassword;
+    private string passwordEncoded;
+    private string url;
+
+    public void LoginAttempt()
+    {
+        string email = InputFieldEmail.GetComponent<InputField>().text;
+        string password = InputFieldPassword.GetComponent<InputField>().text;
+        passwordEncoded = HashFunction.Sha256(password);
+        url = "https://se4med.unibg.it/se4medservice/?action=authenticatepatient&useremail=" + email + "&password=" + passwordEncoded + "&idapp=Runeye";
+    }
+
+
+
+}
