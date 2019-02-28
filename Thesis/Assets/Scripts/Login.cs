bb141d65d08a3ac6dfce404a63703929f4d74e59
Luca Assolari <luca.assolari405@gmail.com>
2019-02-28 18:15:55 +0100

    Update Login.cs

--- Thesis/Assets/Scripts/Login.cs
+++ Thesis/Assets/Scripts/Login.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +11,8 @@ public class Login : MonoBehaviour
     public GameObject InputFieldPassword;
     private string passwordEncoded;
     private string url;
+    private static string result;
+    private string loginFailed = "Served at: /se4medservice{authenticationPatient=Authentication failed. Wrong credential or no user found.}";
 
     public void LoginAttempt()
     {
@@ -16,6 +20,28 @@ public class Login : MonoBehaviour
         string password = InputFieldPassword.GetComponent<InputField>().text;
         passwordEncoded = HashFunction.Sha256(password);
         url = "https://se4med.unibg.it/se4medservice/?action=authenticatepatient&useremail=" + email + "&password=" + passwordEncoded + "&idapp=Runeye";
+        result = Get(url);
+        CheckInfos(result);
+    }
+
+    public string Get(string url)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using (Stream stream = response.GetResponseStream())
+        using (StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    public void CheckInfos(string result)
+    {
+        if (result.Equals(loginFailed))
+            Debug.Log("NOPE");
+        
     }
 
 
